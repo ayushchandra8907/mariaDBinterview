f3fb1445ef4dd87960c6ba06a50251ca52135035
Ayush Chandra <71286983+ayushchandra8907@users.noreply.github.com>
2026-02-02 19:15:34 -0600

    Update hash_table.h

--- include/hash_table.h
+++ include/hash_table.h
@@ -1,1 +1,18 @@
-fasdfasdf
+#pragma once
+
+#include <string>
+
+class HashTable {
+public:
+    //constructor
+    HashTable(size_t size);
+
+
+
+
+    //already provided
+    unsigned int hash(std::string &key, int size); 
+
+private:
+    size_t size;
+};
